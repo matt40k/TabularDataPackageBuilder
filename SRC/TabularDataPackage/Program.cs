bf6e28620767cc8317ba6f082bf88ec81260cc43
Matt Smith <matt@matt40k.co.uk>
2015-06-07 22:18:23 +0100

    Start of application
    Main skeleton of application

---
+++ SRC/TabularDataPackage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace TabularDataPackage
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                // Run User interface
+                Application ui = new Application(); 
+                ui.Run(new UserInterface()); 
+
+            }
+            else
+            {
+                // Run in console mode
+            }
+
+        }
+    }
+}
---
+++ SRC/TabularDataPackage/UserInterface.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace TabularDataPackage
+{
+    /// <summary>
+    /// Interaction logic for UserInterface.xaml
+    /// </summary>
+    public partial class UserInterface : Window
+    {
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+    }
+}
