7f00ec5fe09158803ad15df3e9463f270a10e2c2
Matt Smith <matt@matt40k.co.uk>
2015-06-07 22:13:19 +0100

    License object class
    Object class for Open Definition of Licenses -
    http://opendefinition.org/licenses/

---
+++ SRC/License/License.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class License
+{
+    [DataMember]
+    public bool Domain_Content { get; set; }
+
+    [DataMember]
+    public bool Domain_Data { get; set; }
+
+    [DataMember]
+    public bool Domain_Software { get; set; }
+
+    [DataMember]
+    public string Family { get; set; }
+
+    [DataMember]
+    public string Id { get; set; }
+
+    [DataMember]
+    public string Maintainer { get; set; }
+
+    [DataMember]
+    public string OD_Conformance { get; set; }
+
+    [DataMember]
+    public string OSD_Conformance { get; set; }
+
+    [DataMember]
+    public string Status { get; set; }
+
+    [DataMember]
+    public string Title { get; set; }
+
+    [DataMember]
+    public string Url { get; set; }
+}
