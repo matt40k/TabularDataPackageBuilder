edd9c0a9810401405fbb5712bcde3c86450a47f6
Matt Smith <matt@matt40k.co.uk>
2015-06-08 14:25:10 +0100

    Add select directory to UI

--- SRC/TabularDataPackage/UserInterface.xaml.cs
+++ SRC/TabularDataPackage/UserInterface.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
 
 namespace TabularDataPackage
 {
@@ -8,9 +11,53 @@ namespace TabularDataPackage
     /// </summary>
     public partial class UserInterface : Window
     {
+        private readonly FolderBrowserDialog openFileDialog;
+
         public UserInterface()
         {
             InitializeComponent();
+            openFileDialog = new FolderBrowserDialog();
+        }
+
+        private void buttonBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            string defaultDir = GetGitHubDirectory;
+            if (string.IsNullOrEmpty(defaultDir))
+                dialog.RootFolder = Environment.SpecialFolder.MyDocuments;
+            else
+                dialog.SelectedPath = defaultDir;
+            DialogResult result = dialog.ShowDialog();
+            pathBox.Text = dialog.SelectedPath;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pathBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Directory.Exists(pathBox.Text))
+            {
+                button.IsEnabled = true;
+            }
+            else
+            {
+                button.IsEnabled = false;
+            }
+        }
+
+        public string GetGitHubDirectory
+        {
+            get
+            {
+                string githubDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "GitHub");
+                if (Directory.Exists(githubDir))
+                    return githubDir;
+                else
+                    return null;
+            }
         }
     }
 }
