ca0222506c5ca70783484b7550dd054009039c58
Matt Smith <matt@matt40k.co.uk>
2015-06-09 22:38:18 +0100

    Working license json

--- SRC/TabularDataPackage/GetLicenseJson.ps1
+++ SRC/TabularDataPackage/GetLicenseJson.ps1
@@ -1,21 +1,18 @@
 ﻿$scriptpath = Split-Path $MyInvocation.MyCommand.Path
 $url = "http://licenses.opendefinition.org/licenses/groups/all.json"
 
-
 #if (!(Test-Path $scriptpath\License.json))
 #{
     Invoke-WebRequest $url -OutFile $scriptpath\License.json
 #}
 
 $content = (Get-Content $scriptpath\License.json)
-$content = $content.Replace("    ", "`t`t")
 
-#$content = $content.Replace(""": {", """,") 
+$content = $content| select -Skip 1
+$content = $content.Replace("    ", "`t`t`t")
 $content = $content.Replace(""": {", """,")
-$content = $content.Replace("  """, "`t{`n`t`t""License"": {`n`t`t""name"": """)
-#$content = $content.Replace("  ", "`t")
-#$content = $content.Replace("},", "} },") 
-
-
-#$content = "[" + $content + "]"
+$content = $content.Replace("  """, "`t{`n`t`t""License"": {`n`t`t`t""name"": """)
+$content = $content.Replace("  },", "`t`t}`n`t},")
+$content = $content.Replace("  ", "`t")
+$content = "[" + $content + "]"
 $content | Set-Content $scriptpath\License.json
