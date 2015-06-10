437ef8b2b835155af25435c75fd5cc2e7abf5031
Matt Smith <matt@matt40k.co.uk>
2015-06-10 20:21:34 +0100

    Started building version class - inc UnitTests :)
    Started building version class - inc UnitTests!!
    
    :) :) :)

---
+++ SRC/TabularDataPackage.Test/UnitTest_Versioning.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TabularDataPackage;
+
+namespace TabularDataPackage.Test
+{
+    [TestClass]
+    public class UnitTest_Versioning
+    {
+        [TestMethod]
+        public void VersioningIsNullReturn01()
+        {
+            TabularDataPackage.Versioning versioning = new TabularDataPackage.Versioning();
+            Assert.AreEqual(new Version("0.1"), versioning.GetVersion);
+        }
+
+        [TestMethod]
+        public void VersioningIs11()
+        {
+            TabularDataPackage.Versioning versioning = new TabularDataPackage.Versioning();
+            versioning.SetVersion("1.1");
+            Assert.AreEqual(new Version("1.1"), versioning.GetVersion);
+        }
+    }
+}
---
+++ SRC/TabularDataPackage/Versioning.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TabularDataPackage
+{
+    public class Versioning
+    {
+        private Version _version;
+
+        public Version GetVersion
+        {
+            get
+            {
+                if (_version == null)
+                    return new Version("0.1");
+                return _version;
+            }
+        }
+
+        public bool SetVersion(string version)
+        {
+            _version = new Version(version);
+            return true;
+        }
+    }
+}
