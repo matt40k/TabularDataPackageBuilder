edeee4000ea7f33c4a1644fa790651522a8f92ec
Matt Smith <matt@matt40k.co.uk>
2015-06-11 20:30:20 +0100

    Create test class for csv

---
+++ SRC/TabularDataPackage.Test/UnitTest_Csv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TabularDataPackage;
+
+namespace TabularDataPackage.Test
+{
+    /// <summary>
+    /// Summary description for UnitTest_Csv
+    /// </summary>
+    [TestClass]
+    public class UnitTest_Csv
+    {
+        public UnitTest_Csv()
+        {
+
+        }
+
+        [TestMethod]
+        public void Csv()
+        {
+            TabularDataPackage.Csv versioning = new TabularDataPackage.Csv();
+            Assert.AreEqual(0,0);
+        }
+
+    }
+}
