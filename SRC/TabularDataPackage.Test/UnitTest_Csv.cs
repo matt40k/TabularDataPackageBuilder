d64e803db89cd7aba8123fec21145c85baa69c43
Matt Smith <matt@matt40k.co.uk>
2015-06-11 21:02:20 +0100

    unit tests for hash and utf8

--- SRC/TabularDataPackage.Test/UnitTest_Csv.cs
+++ SRC/TabularDataPackage.Test/UnitTest_Csv.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TabularDataPackage;
 
@@ -16,11 +18,51 @@ namespace TabularDataPackage.Test
         }
 
         [TestMethod]
-        public void Csv()
+        public void CsvValidSHA1Hash()
         {
-            TabularDataPackage.Csv versioning = new TabularDataPackage.Csv();
-            Assert.AreEqual(0,0);
+            TabularDataPackage.Csv _csv = new TabularDataPackage.Csv();
+            string contents = "This is a test file. Please delete me.";
+            string tempFile = Path.GetTempFileName();
+            File.WriteAllText(tempFile, contents);
+            string hash = _csv.GetSHA1Hash(tempFile);
+            File.Delete(tempFile);
+            Assert.AreEqual("CC5872242874C1D33B6B41F37279321D6959F34C", hash);
         }
 
+        [TestMethod]
+        public void CsvInvalidSHA1Hash()
+        {
+            TabularDataPackage.Csv _csv = new TabularDataPackage.Csv();
+            string contents = "This is a test file. Please delete me.";
+            string tempFile = Path.GetTempFileName();
+            File.WriteAllText(tempFile, contents);
+            string hash = _csv.GetSHA1Hash(tempFile);
+            File.Delete(tempFile);
+            Assert.AreNotEqual("NotTheHash", hash);
+        }
+
+        [TestMethod]
+        public void CsvIsUTF8()
+        {
+            TabularDataPackage.Csv _csv = new TabularDataPackage.Csv();
+            string contents = "This is a test file. Please delete me.";
+            string tempFile = Path.GetTempFileName();
+            File.WriteAllText(tempFile, contents, Encoding.UTF8);
+            bool isUTF8 = _csv.GetIsUTF8(tempFile);
+            File.Delete(tempFile);
+            Assert.AreEqual(true, isUTF8);
+        }
+
+        [TestMethod]
+        public void CsvIsUnicode()
+        {
+            TabularDataPackage.Csv _csv = new TabularDataPackage.Csv();
+            string contents = "This is a test file. Please delete me.";
+            string tempFile = Path.GetTempFileName();
+            File.WriteAllText(tempFile, contents, Encoding.Unicode);
+            bool isUTF8 = _csv.GetIsUTF8(tempFile);
+            File.Delete(tempFile);
+            Assert.AreNotEqual(true, isUTF8);
+        }
     }
 }
--- SRC/TabularDataPackage/Csv.cs
+++ SRC/TabularDataPackage/Csv.cs
@@ -1,8 +1,69 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace TabularDataPackage
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Csv
     {
+        /// <summary>
+        /// Returns the SHA1 checksum for the defined file
+        /// </summary>
+        /// <param name="file">file path</param>
+        /// <returns>SHA1</returns>
+        public string GetSHA1Hash(string file)
+        {
+            StringBuilder formatted;
+            using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                using (SHA1Managed sha1 = new SHA1Managed())
+                {
+                    byte[] hash = sha1.ComputeHash(bs);
+                    formatted = new StringBuilder(2 * hash.Length);
+                    foreach (byte b in hash)
+                    {
+                        formatted.AppendFormat("{0:X2}", b);
+                    }
+                }
+            }
+            return formatted.ToString();
+        }
+
+        /// <summary>
+        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
+        /// Defaults to ASCII when detection of the text file's endianness fails.
+        /// </summary>
+        /// <param name="filePath">The text file to analyze.</param>
+        /// <returns>The detected encoding.</returns>
+        public bool GetIsUTF8(string filePath)
+        {
+            // Source: http://stackoverflow.com/questions/3825390/effective-way-to-find-any-files-encoding
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            // Analyze the BOM
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the file size (bytes)
+        /// </summary>
+        /// <param name="filePath">File path</param>
+        /// <returns>bytes</returns>
+        public long GetFileSizeInBytes(string filePath)
+        {
+            FileInfo _fileInfo = new FileInfo(filePath);
+            return _fileInfo.Length;
+        }
     }
 }
