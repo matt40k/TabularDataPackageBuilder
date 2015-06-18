ac10822df519daab4682ebda2f5ed523344b9f93
Matt Smith <matt@matt40k.co.uk>
2015-06-18 23:32:08 +0100

    More work

--- SRC/TabularDataPackage/CsvList.cs
+++ SRC/TabularDataPackage/CsvList.cs
@@ -1,7 +1,14 @@
-﻿public class CsvList
+﻿using System.Data;
+
+public class CsvList
 {
     public bool Selected { get; set; }
     public string Filename { get; set; }
     public bool InPackage { get; set; }
 }
 
+public class CsvColumn
+{
+    public string Name { get; set; }
+    public string Type { get; set;  }
+}
